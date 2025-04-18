db647ca1c4d4b6a497a2c24232e458ffe2c1574f
Code-wizard-satyam <satyamkumarcr07@gmail.com>
2025-04-19 00:45:17 +0530

    initial commit

---
+++ test.js
@@ -0,0 +1,21 @@
+console.log("This is Satyam Kumar");
+
+let x = Number(5);
+const b = 5;
+var c = 2;
+
+is_follower = true;
+okha = null;
+
+const person = {
+    name : "Satyam Kumar",
+    clg : "IITP",
+    age : 20,
+    is_pass : true,
+    marks : null,
+};
+
+person.age += 1;
+
+console.log(person.age);
+console.log(person["name"]);
