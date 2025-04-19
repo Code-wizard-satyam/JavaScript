102ef9d32a606a4f272cf7a8bd1e0985fbb9a08d
Code-wizard-satyam <satyamkumarcr07@gmail.com>
2025-04-19 11:34:37 +0530

    2nd commit

---
+++ script_2.js
@@ -0,0 +1,16 @@
+// This is a comment
+/* this is a multiline comment.
+wibyebvibeihbeibievuj */
+
+// Urinary Operator
+
+let a = 5;
+let b = 2;
+
+console.log("a = ", a , " & b = ", b)
+
+console.log("a++ =", a++)
+console.log("a = ", a)
+
+console.log("--b =", --b)
+console.log("b = ", b)
