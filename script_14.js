4d9fce1339960a9f4b028b0f2298bb6d05bfb712
Code-wizard-satyam <satyamkumarcr07@gmail.com>
2025-05-10 22:48:15 +0530

    CallbackHell

--- script_14.js
+++ script_14.js
@@ -1,22 +1,40 @@
-// Async, Callback and promises
+// // Async, Callback and promises
 
 
-console.log("One");
-console.log("Two");
-function hello(){
-    console.log("Hello");
+// console.log("One");
+// console.log("Two");
+// function hello(){
+//     console.log("Hello");
+// };
+// setTimeout(hello, 2000);   // 2 sec = 2000 msec
+
+// console.log("Three");
+// console.log("Four");
+
+// // Callback
+// function sum(a,b){
+//     console.log(a+b);
+// };
+// function calculator(a,b,sumCallback){
+//     sumCallback(a,b);
+// };
+// calculator(1,2,sum);
+
+function getData(dataId, getNextData) {
+    setTimeout(() => {
+        console.log("Data ", dataId);
+        if(getNextData){
+            getNextData();
+        };
+    }, 2000);
 };
-setTimeout(hello, 2000);   // 2 sec = 2000 msec
 
-console.log("Three");
-console.log("Four");
 
-// Callback
-function sum(a,b){
-    console.log(a+b);
-};
-function calculator(a,b,sumCallback){
-    sumCallback(a,b);
-};
-calculator(1,2,sum);
-
+// Callback Hell
+getData(1, () => {
+    getData(2,()=>{
+        getData(3,()=>{
+            getData(4);
+        });
+    });
+});
