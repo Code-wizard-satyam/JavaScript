4ce27575e70209ac8d533974229249c1d95aae4a
Code-wizard-satyam <satyamkumarcr07@gmail.com>
2025-05-10 22:09:26 +0530

    async

--- script_14.js
+++ script_14.js
@@ -1,1 +1,22 @@
 // Async, Callback and promises
+
+
+console.log("One");
+console.log("Two");
+function hello(){
+    console.log("Hello");
+};
+setTimeout(hello, 2000);   // 2 sec = 2000 msec
+
+console.log("Three");
+console.log("Four");
+
+// Callback
+function sum(a,b){
+    console.log(a+b);
+};
+function calculator(a,b,sumCallback){
+    sumCallback(a,b);
+};
+calculator(1,2,sum);
+
