90c9aed59df813230a7653bdb8dc2e3e3ad42378
Code-wizard-satyam <satyamkumarcr07@gmail.com>
2025-05-11 08:45:19 +0530

    async - await

--- script_14.js
+++ script_14.js
@@ -42,51 +42,95 @@
 //     });
 // });
 
-//promises
-function asyncFunc1() {
+// //promises
+// function asyncFunc1() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("some data1");
+//       resolve("success");
+//     }, 4000);
+//   });
+// }
+
+// function asyncFunc2() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("some data2");
+//       resolve("success");
+//     }, 6000);
+//   });
+// }
+
+// console.log("fetching data1");
+// let pr1 = asyncFunc1();
+// pr1.then((res) => {
+//   console.log(res);
+//   console.log("Fetching Data 2....")
+//   let pr2 = asyncFunc2();
+//   pr2.then((res) => {
+//     console.log("some data2");
+//     resolve("success");
+//   })  
+// });
+
+
+// function getData(id) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log("Data", id);
+//             resolve();
+//         }, 1000);
+//     });
+// }
+
+// // Promise Chain
+// getData(1).then(() => 
+//     getData(2)).then(() => 
+//     getData(3)).then(() => 
+//     getData(4)).catch((error) => 
+//     console.error("Error:", error));
+
+async function myFUn(){
+  console.log("Hello");
+};
+
+function api() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      console.log("some data1");
-      resolve("success");
-    }, 4000);
+      console.log("weather data");
+      resolve(200);
+    }, 2000);
   });
 }
 
-function asyncFunc2() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log("some data2");
-      resolve("success");
-    }, 6000);
-  });
+async function getWeatherData() {
+  await api();    //first
+  await api();    //second
+  await api();    //third
 }
-
-console.log("fetching data1");
-let pr1 = asyncFunc1();
-pr1.then((res) => {
-  console.log(res);
-  console.log("Fetching Data 2....")
-  let pr2 = asyncFunc2();
-  pr2.then((res) => {
-    console.log("some data2");
-    resolve("success");
-  })  
-});
 
 
 function getData(id) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log("Data", id);
-            resolve();
-        }, 1000);
-    });
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log(`Fetched data for ID: ${id}`);
+      resolve(`Data for ${id}`);
+    }, 1000);
+  });
 }
 
-// Promise Chain
-getData(1).then(() => 
-    getData(2)).then(() => 
-    getData(3)).then(() => 
-    getData(4)).catch((error) => 
-    console.error("Error:", error));
+// Async-Await Example
+async function getAllData() {
+  console.log("getting data.....");
 
+  await getData(1);
+  await getData(2);
+  await getData(3);
+  await getData(4);
+  await getData(5);
+  await getData(6);
+
+  console.log("All data fetched!");
+}
+
+getAllData();
